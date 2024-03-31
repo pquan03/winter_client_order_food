07d905bf3a1df4b602c2f0abcfe54396e7cb8e5f
phanquan0712 <phanhongquan>
2024-03-31 17:17:13 +0700

    feat: bottom bar, app bar, custom container

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/route_manager.dart';
+import 'package:winter_food/constants/constants.dart';
+import 'package:winter_food/views/entrypoint.dart';
+
+void main() => runApp(const MyApp());
+
+Widget defaultHome = MainScreen();
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_, child) {
+        return GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Winter Foodly',
+          theme: ThemeData(
+              scaffoldBackgroundColor: kOffWhite,
+              iconTheme: const IconThemeData(color: kDark),
+              primarySwatch: Colors.grey),
+          home: child,
+        );
+      },
+      child: defaultHome,
+    );
+  }
+}
---
+++ lib/views/entrypoint.dart
@@ -0,0 +1,76 @@
+// ignore_for_file: must_be_immutable
+
+import 'package:flutter/material.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
+import 'package:get/get.dart';
+import 'package:winter_food/constants/constants.dart';
+import 'package:winter_food/controllers/tab_index_controller.dart';
+import 'package:winter_food/views/cart/cart_page.dart';
+import 'package:winter_food/views/home/home_page.dart';
+import 'package:winter_food/views/profile/profile_page.dart';
+import 'package:winter_food/views/search/search_page.dart';
+
+class MainScreen extends StatelessWidget {
+  MainScreen({super.key});
+
+  List<Widget> pageList = [
+    const HomePage(),
+    const SearchPage(),
+    const CartPage(),
+    const ProfilePage()
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.put(TabIndexController());
+    return Obx(
+      () => Scaffold(
+        body: Stack(
+          children: [
+            pageList[controller.tabIndex],
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Theme(
+                  data: Theme.of(context).copyWith(
+                    canvasColor: kPrimary,
+                  ),
+                  child: BottomNavigationBar(
+                      showSelectedLabels: false,
+                      showUnselectedLabels: false,
+                      unselectedIconTheme:
+                          const IconThemeData(color: Colors.black38),
+                      selectedIconTheme: const IconThemeData(color: kWhite),
+                      currentIndex: controller.tabIndex,
+                      onTap: (value) {
+                        controller.setTabIndex = value;
+                      },
+                      items: [
+                        BottomNavigationBarItem(
+                            icon: controller.tabIndex == 0
+                                ? const Icon(AntDesign.appstore1)
+                                : const Icon(AntDesign.appstore_o),
+                            label: 'Home'),
+                        const BottomNavigationBarItem(
+                            icon: Icon(Icons.search), label: 'Search'),
+                        const BottomNavigationBarItem(
+                            icon: Badge(
+                                label: Text('1',
+                                    style: TextStyle(
+                                        color: kWhite,
+                                        fontSize: 10,
+                                        fontWeight: FontWeight.bold)),
+                                child: Icon(FontAwesome.opencart)),
+                            label: 'Cart'),
+                        BottomNavigationBarItem(
+                            icon: controller.tabIndex == 3
+                                ? const Icon(FontAwesome.user_circle)
+                                : const Icon(FontAwesome.user_circle_o),
+                            label: 'Profile'),
+                      ])),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
